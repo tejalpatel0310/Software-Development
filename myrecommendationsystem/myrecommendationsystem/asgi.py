953c18efd5bf6c8de7d80392f0cd9182bb63bd00
tejalpatel0310 <pateltejal6079@gmail.com>
2022-03-23 13:04:57 -0600

    commit

--- myrecommendationsystem/myrecommendationsystem/asgi.py
+++ myrecommendationsystem/myrecommendationsystem/asgi.py
@@ -2,7 +2,7 @@
 ASGI config for myrecommendationsystem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
-
+ASGI
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
--- myrecommendationsystem/myrecommendationsystem/wsgi.py
+++ myrecommendationsystem/myrecommendationsystem/wsgi.py
@@ -5,6 +5,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+
+application
 """
 
 import os
