9d66e0f298087fae3b66bddc3de746ebb572960b
tejalpatel0310 <pateltejal6079@gmail.com>
2022-03-23 13:34:52 -0600

    Automatic configuration with django-heroku

--- myrecommendationsystem/myrecommendationsystem/settings.py
+++ myrecommendationsystem/myrecommendationsystem/settings.py
@@ -12,7 +12,8 @@ chanaged
 """
 
 import os
-
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
